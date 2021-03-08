fd9b7be1ee15bfaea700569ea9158060e13bde08
Nodier <so.sistemas@gecamacho.co>
2021-03-08 16:15:50 +0000

    Modulo libro

--- custom_libro/__manifest__.py
+++ custom_libro/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account.move.py'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
