5f33802421a31ad5b828a1576805098dbd26dcfc
Nodier <so.sistemas@gecamacho.co>
2021-03-08 16:46:44 +0000

    Modulo libro

--- custom_libro/__manifest__.py
+++ custom_libro/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['account'],
 
     # always loaded
     'data': [
@@ -31,5 +31,4 @@
         'demo/demo.xml',
     ],
 }
-    
-}
+
