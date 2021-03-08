ea417a63e9fd7619fe527b36c527fe3cd8d7a785
Nodier <so.sistemas@gecamacho.co>
2021-03-08 19:50:40 +0000

    Modulo libro [B

--- custom_libro/models/models.py
+++ custom_libro/models/models.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
- from odoo import models, fields, api
+from odoo import models, fields, api
 
 
- class custom_libro(models.Model):
+class custom_libro(models.Model):
+    _name = 'diario'
     _inherit = 'account.journal'
     
-    libro = field.Many2one('account.journal', string='Libro', required=True, readonly=True,
-        states={'draft': [('readonly', False)]}, domain="[('company_id', '=', company_id)]",
-        default=_get_default_journal)
+    libro = fields.Many2one('account.journal',widget='selection', domain="[('company_id', '=', company_id)]")
     
         
