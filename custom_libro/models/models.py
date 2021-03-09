6e3f20fd70091c5ac6dd643071283927f3df996f
Nodier <so.sistemas@gecamacho.co>
2021-03-09 15:36:18 +0000

    Modulo prueba

--- custom_libro/models/models.py
+++ custom_libro/models/models.py
@@ -5,6 +5,27 @@ from odoo import models, fields, api
 class custom_libro(models.Model):
     _inherit = 'account.move'
     
-    libro = fields.Char(string='Diario Contable')
+    #libro = fields.Char(string='Diario Contable')
     
-   
+    # CLIENTES FACTURAS (VENTAS)
+    #libro = fields.Many2one('account.journal', string='Diario Cont', domain="['&',['company_id', '=', company_id],['type','=','sale']]")
+    
+    #company_id = self._context.get('force_company', self._context.get('default_company_id', self.env.company.id))
+    
+    # PROVEEDOR FACTURAS (COMPRAS)
+    #libro = fields.Many2one('account.journal', string='Diario Cont', domain="['&',['company_id', '=', company_id],['type','=','purchase']]")
+    
+    #raise Warning ("Que tipo de documento es?: ")
+    # PAGOS (BANCOS - EFECTIVO)
+    libro = fields.Many2one('account.journal', string='Diario Cont', domain="['&',['company_id', '=', company_id], '|',['type','=','cash'],['type','=','bank']]")
+    
+    tipo = fields.Char(string='Tipo')
+    
+    # RECEPCIONES (VARIOS - BANCOS - EFECTIVO)
+    #libro = fields.Many2one('account.journal', string='Diario Cont', domain="['&',['company_id', '=', company_id], '|','|',['type','=','general'],['type','=','bank'],['type','=','cash']]")
+
+    #@api.model 
+    #def _move_type(self):
+    #    move_type = self._context.get('default_type', 'entry')
+    
+    
