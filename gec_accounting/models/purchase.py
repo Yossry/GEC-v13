712e36604bb9557c3f10c6b2968a3e18e88bd03d
Nodier Vasquez <so.sistemas@gecamacho.co>
2021-06-01 10:55:26 -0500

    [FIX] purchase order seq

--- gec_accounting/models/purchase.py
+++ gec_accounting/models/purchase.py
@@ -18,28 +18,28 @@ class PurchaseOrderWarehouse(models.Model):
         if vals.get('name', 'New') == 'New':
             if wh.code == 'HCALL':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'H52':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'H33':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'HD52':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'HDBQ':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'HSA':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'HSM':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
             elif wh.code == 'AKT':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
-                    wh.code.lower())
+                    wh.code.lower()+'p')
         res = super(PurchaseOrderWarehouse, self).create(vals)
         print(res)
         return res
