d57712e1d01fb1c4e62f63985973698508e6913b
Nodier Vasquez <vasqueznap@gmail.com>
2021-07-23 11:54:08 -0500

    [IMP] add journal items button filter by partner and account

--- gec_account/static/src/js/customer_balances.js
+++ gec_account/static/src/js/customer_balances.js
@@ -5,8 +5,11 @@ odoo.define('gec_account.customerBalances', function (require) {
     var QWeb = core.qweb;
 
     var Widget = require('web.Widget');
+    var Context = require('web.Context');
+    var data_manager = require('web.data_manager');
     var widget_registry = require('web.widget_registry');
     var config = require('web.config');
+
 
     var _t = core._t;
 
@@ -18,7 +21,6 @@ odoo.define('gec_account.customerBalances', function (require) {
 
         init: function (parent, params) {
             this.data = params.data;
-            console.log(this.data);
             this._super(parent);
         },
 
@@ -47,6 +49,38 @@ odoo.define('gec_account.customerBalances', function (require) {
 
             }));
 
+            var $journalItemsButton = $content.find('.action_open_journal_items');
+            $journalItemsButton.on('click', function(ev) {
+
+                ev.stopPropagation();
+                data_manager.load_action('gec_account.action_account_moves_line_partner').then(function (action) {
+                    // Change action context to choose a specific date and product(s)
+                    // As grid_anchor is set to now() by default in the data, we need
+                    // to load the action first, change the context then launch it via do_action
+                    // additional_context cannot replace a context value, only add new
+                    //
+                    // in case of kit product, the forecast view show the kit's components
+                    console.log('SELF =>',self.data.account_id)
+                    self._rpc({
+                        model: 'account.move.line',
+                        method: 'search',
+                        args: [[['partner_id','=',self.data.partner_id.data.id]]]
+                    }).then(function (res) {
+                        console.log('ACTION',action)
+                        console.log('RES',res)
+//                        var additional_context = {};
+//                        additional_context.grid_anchor = self.data.delivery_date_grid;
+//                        additional_context.search_default_warehouse_id = [self.data.warehouse_id.data.id];
+//                        action.context = new Context(action.context, additional_context);
+                        action.domain = [
+                            ['partner_id', '=', self.data.partner_id.data.id],
+                            ['account_id', '=', self.data.account_id.data.id]
+
+                        ];
+                        self.do_action(action);
+                    });
+                });
+            });
 
             var options = {
                 content: $content,
